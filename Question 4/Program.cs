6955cc722c876aefc6971f9a737de238116b5474
AtefehMosh <elina.ee1.8532@gmail.com>
2023-04-16 07:52:50 +0430

    Add project files.

---
+++ Question 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Question_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Width");
+            int W = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Heigt");
+            int L = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Length");
+            int H = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The volume is"+Clac(W, L, H));
+
+        }
+        private static double Clac(int x,int y,int z)
+        {
+            double result = x * y * x;
+            return result;
+        }
+    }
+}
